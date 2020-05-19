359df487db7f387a1bae1c02da15bd705deadcc4
Glitch (hello-express) <none>
2020-05-19 02:30:07 +0000

    🛫🚅 Checkpoint ./server.js:16337878/778 ./views/layout/common.pug:16337878/256 ./views/index.pug:16337878/87

--- server.js
+++ server.js
@@ -6,28 +6,13 @@
 const express = require("express");
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
+//Template view engine
+app.set('view engine', 'pug');
+app.set('views', './views');
+//Router
+app.get('/', (req, res) => {
+  res.render('index')
+})
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
