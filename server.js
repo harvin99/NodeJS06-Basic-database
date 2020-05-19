3895db8b7f8b8fc073fd7304b5491fa27332964f
Glitch (hello-express) <none>
2020-05-19 03:15:10 +0000

    👽👡 Checkpoint ./server.js:16337878/262 ./views/todo.pug:16337878/656

--- server.js
+++ server.js
@@ -5,23 +5,24 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
-//For body parser
-app.use(express.urlencoded({extended: false}));
+
 
 //Template view engine
 app.set('view engine', 'pug');
 app.set('views', './views');
+//For body parser
+app.use(express.urlencoded({extended: false }));
+
 //Router
 const toDoList = [];
 app.get("/", (req, res) => {
   res.render('index')
 })
-app.get("/todo", (req, res) => {
+app.get("/todos", (req, res) => {
   res.render('todo', {list: toDoList})
 })
-app.post("/todo", (req, res) => {
-  console.log(req.body.task);
-  toDoList.push(req.body.task)
+app.post("/todos", (req, res) => {
+  toDoList.push(req.body.task);
   res.render('todo', {list: toDoList})
 })
 // listen for requests :)
