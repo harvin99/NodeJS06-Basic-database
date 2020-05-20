318eca06b6d2575d33d3eca484f38298a94b1690
Glitch (hello-express) <none>
2020-05-20 12:26:50 +0000

    🚧🌋 Checkpoint ./server.js:16337878/141 ./views/todo.pug:16337878/746

--- server.js
+++ server.js
@@ -6,32 +6,33 @@
 const express = require("express");
 const app = express();
 
-
 //Template view engine
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.set("view engine", "pug");
+app.set("views", "./views");
 //For body parser
-app.use(express.urlencoded({extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 //Router
-const toDoList = ['Nấu ăn', 'Nấu canh', 'Chơi game'];
+const toDoList = ["Nấu ăn", "Nấu canh", "Chơi game"];
 app.get("/", (req, res) => {
-  res.render('index')
-})
+  res.render("index");
+});
 app.get("/todos", (req, res) => {
   const q = req.query.name;
-  if(q){
+  if (q) {
     const matchedTasks = toDoList.filter(
       task => task.toLowerCase().indexOf(q.toLowerCase()) !== -1
-    )
-    res.render('todo', {list: matchedTasks, lastValue: q})
+    );
+    res.render("todo", { list: matchedTasks, lastValue: q });
+  } else {
+    res.render("todo", { list: toDoList });
   }
-  else
-    {
-      res.render('todo', {list: toDoList});
-    }
+});
+app.post('/todos/create', (req, res) => {
+  const task = req.body.todo;
+  toDoList.push(task);
+  res.redirect('todos')
 })
-
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
